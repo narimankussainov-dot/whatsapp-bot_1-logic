26531cbfb36cae26865935c48f3df99558d46692
narimankussainov-dot <narimankussainov@gmail.com>
2026-02-14 22:46:50 +0500

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,9 @@
 # Вставь токен МЕЖДУ кавычек. Убедись, что это ОДНА длинная строка.
 ACCESS_TOKEN = "EAAUeWsYuTiYBQqjDGnBw9U74l1LbM4RjG5r0275HLZBPFWDTitDd0O2o9PZCZBanHwJgRGGesxKNjl7NdCGBVOzKCwsM631ybcxYxnNiQ3NmsNlROvZCsmstgGsFA6Jx3ZAQ2t7w13X5Ghu2QalMGG4gZBZAGygQSYn5omgXZC6ZBHaIDJaRQacxqZAI6MmnZAnFwZDZD"
 
-PHONE_NUMBER_ID = "ТВОЙ_ID_НОМЕРА" 
+PHONE_NUMBER_ID = "992621413930838" 
 VERIFY_TOKEN = "super_secret_password_123"
 VERSION = "v19.0"
 
 ADMIN_PHONE = "77472126808"
+
