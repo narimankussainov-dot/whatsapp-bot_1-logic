1a57e3c2bedf28d7534cf6ed94f335164d6389b4
narimankussainov-dot <narimankussainov@gmail.com>
2026-02-14 22:33:42 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,10 @@
 # config.py
 
-# Вставь сюда свежий токен!
+# Вставь токен МЕЖДУ кавычек. Убедись, что это ОДНА длинная строка.
 ACCESS_TOKEN = "EAAUeWsYuTiYBQqjDGnBw9U74l1LbM4RjG5r0275HLZBPFWDTitDd0O2o9PZCZBanHwJgRGGesxKNjl7NdCGBVOzKCwsM631ybcxYxnNiQ3NmsNlROvZCsmstgGsFA6Jx3ZAQ2t7w13X5Ghu2QalMGG4gZBZAGygQSYn5omgXZC6ZBHaIDJaRQacxqZAI6MmnZAnFwZDZD"
-PHONE_NUMBER_ID = "992621413930838"
+
+PHONE_NUMBER_ID = "ТВОЙ_ID_НОМЕРА" 
 VERIFY_TOKEN = "super_secret_password_123"
 VERSION = "v19.0"
 
-# Специальный номер для тестов (если Meta требует слать на него)
-# SANDBOX_PHONE = "787054425242"
-MY_REAL_PHONE = "77054425242"
-
-# НОМЕР АДМИНА (ЖЕНЫ) - формат без плюса, например 77015554433
-
 ADMIN_PHONE = "77472126808"
-
-
